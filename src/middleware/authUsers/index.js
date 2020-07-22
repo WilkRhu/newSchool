f38321447e4958505aaeca346adc569aec1c34ef
WilkRhu <wilk.caetano@gmail.com>
2020-07-21 21:28:31 -0300

    modified authUser

--- src/middleware/authUsers/index.js
+++ src/middleware/authUsers/index.js
@@ -7,12 +7,14 @@ const auth =  (req, res, next) => {
 
     if(!token_header) return res.status(401).send({error: "Token não informado!"});
     
-    jwt.verify(token_header, process.env.KEY_SISTEM, async (err, decoded) =>{
+    jwt.verify(token_header, process.env.KEY_CRYPT, async (err, decoded) =>{
         if(err){
             if(err.message === "invalid token"){
                 return res.status(400).json({error: "Token inválido"});
             } else if(err.message === "jwt expired") {
                 return res.status(401).send({error: "Sessão Inválida!"});  
+            } else if (err){
+                return res.status(400).json(err);
             }
         } 
         const { email } = decoded;
