1225d82a4fa7c4af72dd4732abf94d7a3dd66499
WilkRhu <wilk.caetano@gmail.com>
2020-07-30 19:49:52 -0300

    id token function

--- src/utils/createToken/index.js
+++ src/utils/createToken/index.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 
-const createToken = (nome, email, type) => {
-    const token = {nome, email, type};
+const createToken = (id, nome, email, type) => {
+    const token = {id, nome, email, type};
     const newToken =  jwt.sign(token, process.env.KEY_CRYPT || "@wilk2020#", {expiresIn: "7d" });
     return newToken;
 };
