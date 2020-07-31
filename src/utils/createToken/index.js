0db772cc76741d0a6a091b6c3d3dc904601291c2
WilkRhu <wilk.caetano@gmail.com>
2020-07-31 11:30:25 -0300

    modify name for name

--- src/utils/createToken/index.js
+++ src/utils/createToken/index.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 
-const createToken = (id, nome, email, type) => {
-    const token = {id, nome, email, type};
+const createToken = (id, name, email, type) => {
+    const token = {id, name, email, type};
     const newToken =  jwt.sign(token, process.env.KEY_CRYPT || "@wilk2020#", {expiresIn: "7d" });
     return newToken;
 };
