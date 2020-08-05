452236a5e8afe1489547b8e7f9d691e883f65a35
WilkRhu <wilk.caetano@gmail.com>
2020-08-05 10:19:21 -0300

    url_database

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,7 @@ module.exports = {
     production: {
         client: "pg",
         version: "9.6",
-        connection: {
-            host: "127.0.0.1",
-            user: "postgres",
-            password: "123",
-            database: "school1"
-        },
+        connection: process.env.HEROKU_POSTGRESQL_CHARCOAL_URL,
         migrations: {
             directory: __dirname + "/src/config/database/migration"
         },
